c1f2a9ea326ea213023323c42106c15ef31fc1e3
nreg94 <nregent@ucsb.edu>
2023-02-17 15:07:23 -0800

    Added widgets for fire behavior, fuels, and tree life stage. Changed theme.

--- fire/app.R
+++ fire/app.R
@@ -1,36 +1,45 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+library(tidyverse)
 library(shiny)
+library(shinythemes)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
+ui <- fluidPage(theme = shinytheme("superhero"),
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Prescribed Fire Timing On Mt. Laguna"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+      sidebarPanel("Put my widgets here!",
+                   radioButtons(inputId = 'penguin_species',
+                                label = "Choose Black Oak Life-Stage",
+                                choices = c("Adult", "Sapling", "Seedling")),
+                   "Choose a color",
+                   selectInput(inputId = 'pt_color',
+                               label = 'choose your favorite color!',
+                               choices = c('Awesome red!' = 'red',
+                                           'Pretty purple' = 'purple',
+                                           'OOOOORange' = 'orange')),
+                   selectInput("select", label = h3("Fuel Loading"), 
+                               choices = list("1 Hr Fuels" = 1, "10 Hr Fuels" = 2, "100 Hr Fuels" = 3, "1000 Hr Fuels" = 4, "Duff Height" = 5, "Litter Height" = 6, "Total Fuels" = 7), 
+                               selected = 1),
+                   
+                   hr(),
+                   fluidRow(column(3, verbatimTextOutput("value"))),
+                   selectInput("select", label = h3("Fire Behavior"), 
+                               choices = list("Char Height" = 1, "Scorch Height" = 2, "Scorch Percent" = 3, "Torch Percent" = 4), 
+                               selected = 1),
+                   
+                   hr(),
+                   fluidRow(column(3, verbatimTextOutput("value")))
+      ), ### End Sidebar Panel
 
         # Show a plot of the generated distribution
         mainPanel(
            plotOutput("distPlot")
-        )
-    )
-)
+        ) ### End Main Panel
+    ) ### End sidebar Layout
+) ### end fluidPage
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
