aa8a5af076d07cb5c72b6825e0672571faf2d1e3
nreg94 <nregent@ucsb.edu>
2023-03-02 23:57:06 -0800

    Changed to tabs, added radiobuttons to tab 1

--- fire/app.R
+++ fire/app.R
@@ -8,37 +8,21 @@ ui <- fluidPage(theme = shinytheme("superhero"),
     # Application title
     titlePanel("Prescribed Fire Timing On Mt. Laguna"),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-      sidebarPanel("Put my widgets here!",
-                   radioButtons(inputId = 'penguin_species',
-                                label = "Choose Black Oak Life-Stage",
-                                choices = c("Adult", "Sapling", "Seedling")),
-                   "Choose a color",
-                   selectInput(inputId = 'pt_color',
-                               label = 'choose your favorite color!',
-                               choices = c('Awesome red!' = 'red',
-                                           'Pretty purple' = 'purple',
-                                           'OOOOORange' = 'orange')),
-                   selectInput("select", label = h3("Fuel Loading"), 
-                               choices = list("1 Hr Fuels" = 1, "10 Hr Fuels" = 2, "100 Hr Fuels" = 3, "1000 Hr Fuels" = 4, "Duff Height" = 5, "Litter Height" = 6, "Total Fuels" = 7), 
-                               selected = 1),
-                   
-                   hr(),
-                   fluidRow(column(3, verbatimTextOutput("value"))),
-                   selectInput("select", label = h3("Fire Behavior"), 
-                               choices = list("Char Height" = 1, "Scorch Height" = 2, "Scorch Percent" = 3, "Torch Percent" = 4), 
-                               selected = 1),
-                   
-                   hr(),
-                   fluidRow(column(3, verbatimTextOutput("value")))
-      ), ### End Sidebar Panel
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        ) ### End Main Panel
-    ) ### End sidebar Layout
+        # Tabs of our different widgets
+          tabsetPanel(type = "tabs",
+                      tabPanel("Trees",
+                               sidebarLayout(
+                                 sidebarPanel(radioButtons(inputId = 'penguin_species',
+                                                           label = "Choose Black Oak Life-stage",
+                                                           choices = c("Adult", "Sapling", "Seedling"))),
+                                 mainPanel(
+                                   htmlOutput("distPlot")
+                                 )
+                               )
+                               ),
+                      tabPanel("Fuel Loading"),
+                      tabPanel("Fire Behavior")
+                      )
 ) ### end fluidPage
 
 # Define server logic required to draw a histogram
